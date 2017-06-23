20556dd9076fd9341ea297f20ff40b5446afb37b
nizamudeenms <nizam007>
2017-06-23 13:14:35 +0300

    Started Stage 2.
    Added Youtube fragment...
    hardcoded video id..
    
    -Nizam

--- app/src/main/java/com/example/nizamudeenms/myflikz/DetailActivity.java
+++ app/src/main/java/com/example/nizamudeenms/myflikz/DetailActivity.java
@@ -1,29 +1,37 @@
 package com.example.nizamudeenms.myflikz;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerFragment;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 /**
  * Created by nizamudeenms on 26/03/17.
  */
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity implements YouTubePlayer.OnInitializedListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        YouTubePlayerFragment youTubePlayerFragment;
 
         ImageView posterImageView;
         ImageView backDropImageView;
         TextView movieNameTextView;
         TextView overviewTextView, ratingTextView,releaseDateTextView;
 
-
+        youTubePlayerFragment = (YouTubePlayerFragment)getFragmentManager().findFragmentById(R.id.youtube_playerviewfragment);
+        youTubePlayerFragment.initialize(VideoPlayerConfig.API_KEY,this);
         posterImageView = (ImageView) findViewById(R.id.poster);
         backDropImageView = (ImageView) findViewById(R.id.back_drop);
         movieNameTextView = (TextView) findViewById(R.id.movie_name);
@@ -45,4 +53,32 @@ public class DetailActivity extends AppCompatActivity {
         ratingTextView.setText(rating);
     }
 
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        if (!b) {
+            youTubePlayer.cueVideo(VideoPlayerConfig.VIDEO_ID);
+            youTubePlayer.cueVideo("s4C1gnqdrew");
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        if (youTubeInitializationResult.isUserRecoverableError()) {
+            youTubeInitializationResult.getErrorDialog(this, 1).show();
+        } else {
+            String errorMessage = String.format("Eror in youtube api",youTubeInitializationResult.toString());
+            Toast.makeText(this,errorMessage,Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1) {
+            getYoutubePlayerProvider().initialize(VideoPlayerConfig.API_KEY,this);
+        }
+    }
+
+    protected YouTubePlayer.Provider getYoutubePlayerProvider(){
+        return (YouTubePlayerView) findViewById(R.id.youtube_playerviewfragment);
+    }
 }
