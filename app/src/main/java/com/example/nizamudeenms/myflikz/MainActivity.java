675c227ebbca2b25690a4e68d9f554e2f7e423c2
nizamudeenms <nizam007>
2017-10-23 23:40:28 +0300

    Replaced Volley with Okhttp...
    Favorites movie part added buy not yet completed.
    
    -Nizam

--- app/src/main/java/com/example/nizamudeenms/myflikz/MainActivity.java
+++ app/src/main/java/com/example/nizamudeenms/myflikz/MainActivity.java
@@ -17,26 +17,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.NetworkError;
-import com.android.volley.NoConnectionError;
-import com.android.volley.ParseError;
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.ServerError;
-import com.android.volley.TimeoutError;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
-    private String TAG = MainActivity.class.getSimpleName();
+    private String TAG = "DROGON" + MainActivity.class.getSimpleName();
     private static final String endpoint = "http://image.tmdb.org/t/p/";
     private ArrayList<Movie> movies;
     private ProgressDialog pDialog;
@@ -54,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         movies = new ArrayList<>();
+        Context context = this;
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
 
@@ -62,27 +55,31 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setHasFixedSize(true);
 
+
         MovieDbHelper movieDbHelper = new MovieDbHelper(this);
         mMovieDb = movieDbHelper.getWritableDatabase();
 
+
+        FetchMoviesTask fetchMovies = new FetchMoviesTask();
+        fetchMovies.execute();
+
+        FetchMoviesTask topMoviesTask = new FetchMoviesTask();
+        topMoviesTask.execute();
+
         Cursor cPopularMovies = getPopularMovies();
         Cursor cTopMovies = getTopMovies();
+        Log.i(TAG, "Adapter Set");
 
+        System.out.println("sortBy : " + sortBy);
         if (sortBy.equals(GET_POPULAR)) {
             System.out.println("inside popular movies");
             mAdapter = new MovieAdapter(getApplicationContext(), cPopularMovies);
             recyclerView.setAdapter(mAdapter);
-        }else if(sortBy.equals(GET_TOP)){
+        } else if (sortBy.equals(GET_TOP)) {
             System.out.println("inside top movies");
             mAdapter = new MovieAdapter(getApplicationContext(), cTopMovies);
             recyclerView.setAdapter(mAdapter);
         }
-
-
-
-        Log.i(TAG, "Adapter Set");
-        FetchMoviesTask fetchMovies = new FetchMoviesTask();
-        fetchMovies.execute();
 
 
     }
@@ -117,147 +114,131 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected Void doInBackground(Void... params) {
             final String FORECAST_BASE_URL = "https://api.themoviedb.org/3/movie/";
-
-            String url = FORECAST_BASE_URL + GET_POPULAR + "?api_key=" + BuildConfig.TMDB_KEY;
-
 //        pDialog.setMessage("Downloading json...");
 //        pDialog.show();
 
-            if (sortBy.equals(GET_POPULAR)) {
-                url = FORECAST_BASE_URL + GET_POPULAR + "?api_key=" + BuildConfig.TMDB_KEY;
-            } else if (sortBy.equals(GET_TOP)) {
-                url = FORECAST_BASE_URL + GET_TOP + "?api_key=" + BuildConfig.TMDB_KEY;
-            } else {
-                url = FORECAST_BASE_URL + GET_POPULAR + "?api_key=" + BuildConfig.TMDB_KEY;
+            String popular_url = FORECAST_BASE_URL + GET_POPULAR + "?api_key=" + BuildConfig.TMDB_KEY;
+            String top_url = FORECAST_BASE_URL + GET_TOP + "?api_key=" + BuildConfig.TMDB_KEY;
+
+            OkHttpClient client = new OkHttpClient();
+            try {
+                Request request = new Request.Builder()
+                        .url(popular_url)
+                        .build();
+
+                Response response = client.newCall(request).execute();
+                String res = response.body().string();
+                JSONObject jsonObject = new JSONObject(res);
+                JSONArray jsonArray = jsonObject.getJSONArray("results");
+
+                for (int j = 0; j < jsonArray.length(); j++) {
+                    JSONObject c = jsonArray.getJSONObject(j);
+                    String posterPath = c.getString("poster_path");
+                    String backdropPath = c.getString("backdrop_path");
+//                                Log.i(TAG, posterPath);
+                    Log.i(TAG, c.getString("original_title"));
+                    Log.i(TAG, c.getString("vote_average"));
+                    Log.i(TAG, c.getString("release_date"));
+
+                    Movie movie = new Movie();
+                    movie.setPOSTER_PATH(endpoint + "w185/" + posterPath);
+                    movie.setBACKDROP_PATH(endpoint + "w500/" + backdropPath);
+                    movie.setID(c.getString("id"));
+                    movie.setOVERVIEW(c.getString("overview"));
+                    movie.setRELEASE_DATE(c.getString("release_date"));
+                    movie.setTITLE(c.getString("original_title"));
+                    movie.setVOTE_AVERAGE(c.getString("vote_average"));
+                    movies.add(movie);
+
+                    ContentValues cv = new ContentValues();
+                    cv.put(MovieContract.MovieEntry.COLUMN_POSTER_URL, endpoint + "w185/" + posterPath);
+                    cv.put(MovieContract.MovieEntry.COLUMN_BACKDROP_URL, endpoint + "w500/" + backdropPath);
+                    cv.put(MovieContract.MovieEntry.COLUMN_TITLE, c.getString("original_title"));
+                    cv.put(MovieContract.MovieEntry.COLUMN_MOVIE_ID, c.getString("id"));
+                    cv.put(MovieContract.MovieEntry.COLUMN_OVERVIEW, c.getString("overview"));
+                    cv.put(MovieContract.MovieEntry.COLUMN_RELEASE_DATE, c.getString("release_date"));
+                    cv.put(MovieContract.MovieEntry.COLUMN_VOTE_AVERAGE, c.getString("vote_average"));
+                    mMovieDb.insert(MovieContract.MovieEntry.POPULAR_MOVIE_TABLE, null, cv);
+                    System.out.println("Value inserted in Popular DB ");
+                }
+
+                Request request2 = new Request.Builder()
+                        .url(top_url)
+                        .build();
+
+
+                //Top Movies Starting
+                Response response2 = client.newCall(request2).execute();
+                String res2 = response2.body().string();
+                JSONObject jsonObject2 = new JSONObject(res2);
+                JSONArray jsonArray2 = jsonObject2.getJSONArray("results");
+
+                for (int j = 0; j < jsonArray2.length(); j++) {
+                    JSONObject c = jsonArray2.getJSONObject(j);
+                    String posterPath = c.getString("poster_path");
+                    String backdropPath = c.getString("backdrop_path");
+                    Log.i(TAG, c.getString("original_title"));
+                    Log.i(TAG, c.getString("vote_average"));
+                    Log.i(TAG, c.getString("release_date"));
+
+                    Movie movie = new Movie();
+                    movie.setPOSTER_PATH(endpoint + "w185/" + posterPath);
+                    movie.setBACKDROP_PATH(endpoint + "w500/" + backdropPath);
+                    movie.setID(c.getString("id"));
+                    movie.setOVERVIEW(c.getString("overview"));
+                    movie.setRELEASE_DATE(c.getString("release_date"));
+                    movie.setTITLE(c.getString("original_title"));
+                    movie.setVOTE_AVERAGE(c.getString("vote_average"));
+                    movies.add(movie);
+
+                    ContentValues cv = new ContentValues();
+                    cv.put(MovieContract.MovieEntry.COLUMN_POSTER_URL, endpoint + "w185/" + posterPath);
+                    cv.put(MovieContract.MovieEntry.COLUMN_BACKDROP_URL, endpoint + "w500/" + backdropPath);
+                    cv.put(MovieContract.MovieEntry.COLUMN_TITLE, c.getString("original_title"));
+                    cv.put(MovieContract.MovieEntry.COLUMN_MOVIE_ID, c.getString("id"));
+                    cv.put(MovieContract.MovieEntry.COLUMN_OVERVIEW, c.getString("overview"));
+                    cv.put(MovieContract.MovieEntry.COLUMN_RELEASE_DATE, c.getString("release_date"));
+                    cv.put(MovieContract.MovieEntry.COLUMN_VOTE_AVERAGE, c.getString("vote_average"));
+                    mMovieDb.insert(MovieContract.MovieEntry.TOP_MOVIE_TABLE, null, cv);
+                    System.out.println("Value inserted in Top DB ");
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+
+        }
+    }
+            @Override
+            public boolean onCreateOptionsMenu (Menu menu){
+                getMenuInflater().inflate(R.menu.main, menu);
+                return true;
             }
 
-            JsonObjectRequest req = new JsonObjectRequest(Request.Method.GET,
-                    url, null,
-                    new Response.Listener<JSONObject>() {
+            @Override
+            public boolean onOptionsItemSelected (MenuItem item){
+                int menuSelected = item.getItemId();
 
-                        @Override
-                        public void onResponse(JSONObject response) {
-//                            Log.d(TAG, response.toString());
-                            movies.clear();
-                            try {
-                                JSONArray responseBundle = response.getJSONArray("results");
-                                for (int j = 0; j < responseBundle.length(); j++) {
-                                    JSONObject c = responseBundle.getJSONObject(j);
-                                    String posterPath = c.getString("poster_path");
-                                    String backdropPath = c.getString("backdrop_path");
-//                                Log.i(TAG, posterPath);
-                                    Log.i(TAG, c.getString("original_title"));
-                                    Log.i(TAG, c.getString("vote_average"));
-                                    Log.i(TAG, c.getString("release_date"));
-
-                                    Movie movie = new Movie();
-                                    movie.setPOSTER_PATH(endpoint + "w185/" + posterPath);
-                                    movie.setBACKDROP_PATH(endpoint + "w500/" + backdropPath);
-                                    movie.setID(c.getString("id"));
-                                    movie.setOVERVIEW(c.getString("overview"));
-                                    movie.setRELEASE_DATE(c.getString("release_date"));
-                                    movie.setTITLE(c.getString("original_title"));
-                                    movie.setVOTE_AVERAGE(c.getString("vote_average"));
-                                    movies.add(movie);
-
-                                    if (sortBy.equals(GET_POPULAR)) {
-                                        ContentValues cv = new ContentValues();
-                                        cv.put(MovieContract.MovieEntry.COLUMN_POSTER_URL, endpoint + "w185/" + posterPath);
-                                        cv.put(MovieContract.MovieEntry.COLUMN_BACKDROP_URL, endpoint + "w500/" + backdropPath);
-                                        cv.put(MovieContract.MovieEntry.COLUMN_TITLE, c.getString("original_title"));
-                                        cv.put(MovieContract.MovieEntry.COLUMN_MOVIE_ID, c.getString("id"));
-                                        cv.put(MovieContract.MovieEntry.COLUMN_OVERVIEW, c.getString("overview"));
-                                        cv.put(MovieContract.MovieEntry.COLUMN_RELEASE_DATE, c.getString("release_date"));
-                                        cv.put(MovieContract.MovieEntry.COLUMN_VOTE_AVERAGE, c.getString("vote_average"));
-                                        mMovieDb.insert(MovieContract.MovieEntry.POPULAR_MOVIE_TABLE, null, cv);
-                                        System.out.println("Value inserted in Popular DB ");
-                                    } else {
-                                        ContentValues cv = new ContentValues();
-                                        cv.put(MovieContract.MovieEntry.COLUMN_POSTER_URL, endpoint + "w185/" + posterPath);
-                                        cv.put(MovieContract.MovieEntry.COLUMN_BACKDROP_URL, endpoint + "w500/" + backdropPath);
-                                        cv.put(MovieContract.MovieEntry.COLUMN_TITLE, c.getString("original_title"));
-                                        cv.put(MovieContract.MovieEntry.COLUMN_MOVIE_ID, c.getString("id"));
-                                        cv.put(MovieContract.MovieEntry.COLUMN_OVERVIEW, c.getString("overview"));
-                                        cv.put(MovieContract.MovieEntry.COLUMN_RELEASE_DATE, c.getString("release_date"));
-                                        cv.put(MovieContract.MovieEntry.COLUMN_VOTE_AVERAGE, c.getString("vote_average"));
-                                        mMovieDb.insert(MovieContract.MovieEntry.TOP_MOVIE_TABLE, null, cv);
-                                        System.out.println("Value inserted in Top DB ");
-                                    }
-
-
-                                }
-
-                            } catch (JSONException e) {
-                                Log.e(TAG, "Json parsing error: " + e.getMessage());
-                            }
-
-                            mAdapter.notifyDataSetChanged();
-                        }
-                    }, new Response.ErrorListener() {
-
-                @Override
-                public void onErrorResponse(VolleyError volleyError) {
-                    String message = null;
-                    if (volleyError instanceof NetworkError) {
-                        message = "Cannot connect to Internet...Please check your connection!";
-                    } else if (volleyError instanceof ServerError) {
-                        message = "The server could not be found. Please try again after some time!!";
-                    } else if (volleyError instanceof AuthFailureError) {
-                        message = "Cannot connect to Internet...Please check your connection!";
-                    } else if (volleyError instanceof ParseError) {
-                        message = "Parsing error! Please try again after some time!!";
-                    } else if (volleyError instanceof NoConnectionError) {
-                        message = "Cannot connect to Internet...Please check your connection!";
-                    } else if (volleyError instanceof TimeoutError) {
-                        message = "Connection TimeOut! Please check your internet connection.";
-                    }
-
-                    Toast toast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG);
+                if (menuSelected == R.id.popular) {
+                    sortBy = GET_POPULAR;
+                    Cursor cPopularMovies = getPopularMovies();
+                    mAdapter = new MovieAdapter(getApplicationContext(), cPopularMovies);
+                    recyclerView.setAdapter(mAdapter);
+                    Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.popular_sel_mes), Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.TOP | Gravity.CENTER, 0, 0);
                     toast.show();
-                    volleyError.printStackTrace();
+                } else if (menuSelected == R.id.topRated) {
+                    sortBy = GET_TOP;
+                    Cursor cTopMovies = getTopMovies();
+                    mAdapter = new MovieAdapter(getApplicationContext(), cTopMovies);
+                    recyclerView.setAdapter(mAdapter);
+                    Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.top_rated_sel_msg), Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.TOP | Gravity.CENTER, 0, 0);
+                    toast.show();
                 }
-            });
-            MovieController.getInstance().addToRequestQueue(req);
-            return null;
-        }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int menuSelected = item.getItemId();
-
-        if (menuSelected == R.id.popular) {
-            sortBy = GET_POPULAR;
-            FetchMoviesTask fetchMovies = new FetchMoviesTask();
-            fetchMovies.execute();
-            Cursor cPopularMovies = getPopularMovies();
-            mAdapter = new MovieAdapter(getApplicationContext(), cPopularMovies);
-            recyclerView.setAdapter(mAdapter);
-            Context context = this;
-            Toast toast = Toast.makeText(context, getString(R.string.popular_sel_mes), Toast.LENGTH_LONG);
-            toast.setGravity(Gravity.TOP | Gravity.CENTER, 0, 0);
-            toast.show();
-        } else if (menuSelected == R.id.topRated) {
-            sortBy = GET_TOP;
-            FetchMoviesTask fetchMovies = new FetchMoviesTask();
-            fetchMovies.execute();
-            Cursor cTopMovies = getTopMovies();
-            mAdapter = new MovieAdapter(getApplicationContext(), cTopMovies);
-            recyclerView.setAdapter(mAdapter);
-            Context context = this;
-            Toast toast = Toast.makeText(context, getString(R.string.top_rated_sel_msg), Toast.LENGTH_LONG);
-            toast.setGravity(Gravity.TOP | Gravity.CENTER, 0, 0);
-            toast.show();
-        }
-        return true;
-    }
+                return true;
+            }
 
 
 //    @Override
@@ -267,4 +248,4 @@ public class MainActivity extends AppCompatActivity {
 //        Toast.makeText(context, " Activity Launched", Toast.LENGTH_SHORT).show();
 //        startActivity(intent);
 //    }
-}
+        }
